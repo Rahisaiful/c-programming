8c2316662c344bdbf44204b40368dd75228360d1
Saiful Islam <Rahisaiful@users.noreply.github.com>
2023-04-04 00:05:04 +0600

    update lesson

---
+++ module-1/output.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main() {
+
+    // How to get input
+
+    // int rahim,karim;
+    // scanf( "%d %d", &rahim, &karim );
+    // printf( "%d %d", rahim, karim );
+
+
+
+    // How to get input with char, type -1
+    // int rahim,karim;
+    // char p;
+    // scanf( "%d%c %d%c", &rahim,&p, &karim,&p );
+    // printf( "%d%c %d%c", rahim,p, karim,p );
+
+
+
+    // How to get input with char, type -2
+
+    int rahim,karim;
+
+    scanf( "%da %db", &rahim, &karim );
+
+    printf( "%da %db", rahim, karim );
+
+    return 0;
+}
---
+++ module-1/variable.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+    int rahim = 10;
+    float karim = 105.3;
+    char habib = 'z';
+    printf("%d %0.2f %c", rahim, karim, habib );
+
+    return 0;
+}
